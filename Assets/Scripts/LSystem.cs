78ee1a8d5c12051adc8d8edb0aed81351097339d
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-12-02 11:40:57 +0100

    changed meshrenderer with linerenderer instead on draw

--- Assets/Scripts/LSystem.cs
+++ Assets/Scripts/LSystem.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LSystem : MonoBehaviour
 {
     public GameObject branchPrefab;
+    public Material woodMaterial;
     public int n;
     public float angle;
     public string axiom;
@@ -12,10 +13,14 @@ public class LSystem : MonoBehaviour
     private List<Rule> rules;
     private string sentence;
     private Turtle turtle;
+    private GameObject parentGO;
 
     // Start is called before the first frame update
     void Start()
     {
+        parentGO = new GameObject("parentTree");
+        parentGO.transform.position = new Vector3(0, 0, -5);
+
         rules = new List<Rule>();
         turtle = new Turtle(new Vector3(0, 0, -5), 1f, 90f);
 
@@ -56,7 +61,7 @@ public class LSystem : MonoBehaviour
                     Vector3 startPos = turtle.CrtPosition;
                     turtle.Translate();
                     Vector3 endPos = turtle.CrtPosition;
-                    Draw(startPos, endPos);
+                    DrawLine(startPos, endPos);
                     break;
                 case '+':
                     turtle.Rotate(angle);
@@ -78,11 +83,26 @@ public class LSystem : MonoBehaviour
         turtle.MultiplyTranslation(0.75f);
     }
 
-    void Draw(Vector3 startPos, Vector3 endPos)
+    void DrawObject(Vector3 startPos, Vector3 endPos)
     {
         Vector3 position = (startPos + endPos) * 0.5f;
         GameObject branchGO = Instantiate(branchPrefab, position, Quaternion.Euler(0, 0, turtle.CrtRotation - 90f));
         branchGO.transform.localScale = Vector3.one * turtle.CrtTranslation * 0.5f;
+        branchGO.transform.parent = parentGO.transform;
+    }
+
+    void DrawLine(Vector3 startPos, Vector3 endPos)
+    {
+        GameObject branchGO = new GameObject("branch");
+        LineRenderer line = branchGO.AddComponent<LineRenderer>();
+        line.positionCount = 2;
+        line.SetPosition(0, startPos);
+        line.SetPosition(1, endPos);
+        line.material = woodMaterial;
+        line.generateLightingData = true;
+        line.startWidth = 0.5f;
+        line.endWidth = 0.5f;
+        branchGO.transform.parent = parentGO.transform;
     }
 
     string GetSubInRules(char ch)
@@ -97,4 +117,26 @@ public class LSystem : MonoBehaviour
 
         return ch.ToString();
     }
+
+    //void CombineMeshes()
+    //{
+    //    meshFilters = parentGO.GetComponentsInChildren<MeshFilter>();
+    //    combine = new CombineInstance[meshFilters.Length];
+    //    MeshRenderer parentMesh = parentGO.AddComponent<MeshRenderer>();
+    //    parentMesh.material = parentGO.GetComponentInChildren<MeshRenderer>().material;
+
+    //    int i = 0;
+    //    while (i < meshFilters.Length)
+    //    {
+    //        combine[i].mesh = meshFilters[i].sharedMesh;
+    //        combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+    //        meshFilters[i].gameObject.SetActive(false);
+
+    //        i++;
+    //    }
+    //    parentGO.AddComponent<MeshFilter>();
+    //    parentGO.transform.GetComponent<MeshFilter>().mesh = new Mesh();
+    //    parentGO.transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+    //    parentGO.SetActive(true);
+    //}
 }
