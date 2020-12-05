83714ca5d305c5404f6f55ada5667cc334a9e36a
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-12-05 17:25:37 +0100

    camera controller euler rotation on click

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,25 +7,19 @@ public class CameraController : MonoBehaviour
     public float translationSpeed;
     public float rotationSpeed;
 
-    private Camera mainCamera;
-    private Vector3 crtScreenMousePosition;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        mainCamera = GetComponent<Camera>();
-    }
+    private float yaw;
+    private float pitch;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey(KeyCode.Z))
         {
-            Translate(new Vector3(0, 1, 0), translationSpeed * Time.deltaTime);
+            Translate(new Vector3(0, 0, 1), translationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Translate(new Vector3(0, -1, 0), translationSpeed * Time.deltaTime);
+            Translate(new Vector3(0, 0, -1), translationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.Q))
         {
@@ -37,42 +31,29 @@ public class CameraController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Translate(new Vector3(0, 0, 1), translationSpeed * Time.deltaTime);
+            Translate(new Vector3(0, 1, 0), translationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.E))
         {
-            Translate(new Vector3(0, 0, -1), translationSpeed * Time.deltaTime);
+            Translate(new Vector3(0, -1, 0), translationSpeed * Time.deltaTime);
         }
 
         if(Input.GetMouseButton(1))
         {
-            Vector3 newScreenMousePosition = GetScreenMousePosition(100f);
-            if(crtScreenMousePosition != newScreenMousePosition)
-            {
-                crtScreenMousePosition = newScreenMousePosition;
-                LookAt(GetWorldMousePosition(crtScreenMousePosition));
-            }
+            yaw += rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
+            pitch -= rotationSpeed * Time.deltaTime * Input.GetAxis("Mouse Y");
+
+            RotateEuler(pitch, yaw, 0f);
         }
     }
 
-    void LookAt(Vector3 position)
+    void RotateEuler(float pitch, float yaw, float roll)
     {
-        transform.LookAt(position);
+        transform.eulerAngles = new Vector3(pitch, yaw, roll);
     }
 
     void Translate(Vector3 direction, float value)
     {
         transform.Translate(direction * value);
-    }
-
-    Vector3 GetWorldMousePosition(Vector3 screenMousePosition)
-    {
-        return mainCamera.ScreenToWorldPoint(screenMousePosition);
-    }
-
-    Vector3 GetScreenMousePosition(float dst)
-    {
-        Vector3 mousePosition = Input.mousePosition;
-        return new Vector3(mousePosition.x, mousePosition.y, dst);
     }
 }
